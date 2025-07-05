81ac8725bb125dde86907a7c76ee441d31a241f7
Ayyoubez <ayyoubez@gmail.com>
2025-07-05 20:22:12 +0300

    Created a connection to MongoDB

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,10 +3,12 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Header from "./Components/Header";
 export default function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ Client/src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import logo from "/logo.png";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <div className="bg-slate-300 shadow-md">
+      <div className="flex items-center justify-between pt-2 max-w-6xl mx-auto">
+        <img className="w-32" src={logo} alt="Kech Estate Logo" />
+        <div className="flex items-center justify-between bg-slate-100 p-3 rounded-lg mr-1 sm:mr-0">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="focus:outline-none"
+          />
+          <FaSearch className="text-slate-600" />
+        </div>
+        <ul className="flex gap-4 sm:gap-6 text-xl font-bold text-slate-700 pr-3">
+          <Link to={"/"}>
+            <li className="hidden sm:inline hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to={"/sign-up"}>
+            {" "}
+            <li className="hidden cursor-pointer sm:inline hover:underline">
+              Register
+            </li>
+          </Link>
+          <Link to={"/login"}>
+            {" "}
+            <li className="  cursor-pointer sm:inline hover:underline">
+              Login
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
