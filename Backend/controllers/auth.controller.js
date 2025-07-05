0849398531739ff4e06ea10b49acf729b893d483
Ayyoubez <ayyoubez@gmail.com>
2025-07-05 23:33:55 +0300

    created auth api route

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  const { userName, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ userName, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(500).json("success");
+  } catch (error) {
+    res.json(error.message);
+  }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -16,6 +18,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(5000, () => {
   console.log(" server running on 5000...");
