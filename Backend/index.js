94a2f7f6a9d634e0e6ddc9929a988baf8219dbe0
Ayyoubez <ayyoubez@gmail.com>
2025-07-05 22:46:12 +0300

    created user route

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -14,9 +15,7 @@ mongoose
     console.log(err);
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use("/api/user", userRouter);
 
 app.listen(5000, () => {
   console.log(" server running on 5000...");
